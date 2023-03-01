82aad95d361be4fe17097e655ae9b7964e0f0be0
mrnikola01 <vukosavljevicn.va@gmail.com>
2023-03-01 19:30:16 +0100

    Added gameRounds score to be 5 and paragraph color changing.

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ function playRound(playerChoice, opponentChoice) {
     opponentScore++;
     opponentResult.textContent = "Opponent result " + opponentScore;
     resultText.textContent = "You lose! The opponent win with Paper.";
+    resultText.style = "color: red;";
   } else if (playerChoice == 0 && opponentChoice == 2) {
     playerChoiceText.textContent = "Your choice: Rock";
     opponentChoiceText.textContent = "Opponent choice: Scissors";
@@ -52,10 +53,12 @@ function playRound(playerChoice, opponentChoice) {
     playerScore++;
     playerResult.textContent = "Your result " + playerScore;
     resultText.textContent = "You win against the opponent with Rock!";
+    resultText.style = "color: green;";
   } else if (playerChoice == 0 && opponentChoice == 0) {
     playerChoiceText.textContent = "Your choice: Rock";
     opponentChoiceText.textContent = "Opponent choice: Rock";
     resultText.textContent = "It is a draw this time!";
+    resultText.style = "color: black;";
   } else if (playerChoice == 1 && opponentChoice == 0) {
     playerChoiceText.textContent = "Your choice: Paper";
     opponentChoiceText.textContent = "Opponent choice: Rock";
@@ -63,6 +66,7 @@ function playRound(playerChoice, opponentChoice) {
     playerScore++;
     playerResult.textContent = "Your result " + playerScore;
     resultText.textContent = "You win against the opponent with Paper.";
+    resultText.style = "color: green;";
   } else if (playerChoice == 1 && opponentChoice == 2) {
     playerChoiceText.textContent = "Your choice: Paper";
     opponentChoiceText.textContent = "Opponent choice: Scissors";
@@ -70,10 +74,12 @@ function playRound(playerChoice, opponentChoice) {
     opponentScore++;
     opponentResult.textContent = "Opponent result " + opponentScore;
     resultText.textContent = "You lose! The opponent win with Scissors.";
+    resultText.style = "color: red;";
   } else if (playerChoice == 1 && opponentChoice == 1) {
     playerChoiceText.textContent = "Your choice: Paper";
     opponentChoiceText.textContent = "Opponent choice: Paper";
     resultText.textContent = "It is a draw this time!";
+    resultText.style = "color: black;";
   } else if (playerChoice == 2 && opponentChoice == 0) {
     playerChoiceText.textContent = "Your choice: Scissors";
     opponentChoiceText.textContent = "Opponent choice: Rock";
@@ -81,6 +87,7 @@ function playRound(playerChoice, opponentChoice) {
     opponentScore++;
     opponentResult.textContent = "Opponent result " + opponentScore;
     resultText.textContent = "You lose! The opponent win with Rock.";
+    resultText.style = "color: red;";
   } else if (playerChoice == 2 && opponentChoice == 1) {
     playerChoiceText.textContent = "Your choice: Scissors";
     opponentChoiceText.textContent = "Opponent choice: Paper";
@@ -88,10 +95,33 @@ function playRound(playerChoice, opponentChoice) {
     playerScore++;
     playerResult.textContent = "Your result " + playerScore;
     resultText.textContent = "You win against the opponent with Scissors.";
+    resultText.style = "color: green;";
   } else if (playerChoice == 2 && opponentChoice == 2) {
     playerChoiceText.textContent = "Your choice: Scissors";
     opponentChoiceText.textContent = "Opponent choice: Scissors";
     resultText.textContent = "It is a draw this time!";
+    resultText.style = "color: black;";
   }
   gameRounds++;
+  if (gameRounds == 5) {
+    if (playerScore > opponentScore) {
+      resultText.textContent =
+        "You won this game! Press a button to play new round.";
+      resultText.style = "color: green;";
+    } else if (opponentScore > playerScore) {
+      resultText.textContent =
+        "You lose this game against the opponent! Press a button to play new round.";
+      resultText.style = "color: red;";
+    } else if (playerScore == opponentScore) {
+      resultText.textContent =
+        "This game has no winners. Press a button to play new round.";
+      resultText.style = "color: black;";
+    }
+
+    (gameRounds = 0), (playerScore = 0), (opponentScore = 0);
+    playerChoiceText.textContent = "Your choice:";
+    opponentChoiceText.textContent = "Your choice:";
+    playerResult.textContent = "Your result: 0";
+    opponentResult.textContent = "Opponent result: 0";
+  }
 }
