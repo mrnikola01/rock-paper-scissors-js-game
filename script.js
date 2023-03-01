8849d1547f3270df999d425ff12695fab42827b5
mrnikola01 <vukosavljevicn.va@gmail.com>
2023-03-01 18:26:53 +0100

    Implemented paragraph for win or lose and added let gameRounds, but without code for that

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let opponentScore = 0;
+let gameRounds = 0;
 
 const options = ["Rock", "Paper", "Scissors"];
 
@@ -12,6 +13,8 @@ const opponentChoiceText = document.querySelector(".opponent-choice");
 
 const playerResult = document.querySelector(".player-result");
 const opponentResult = document.querySelector(".opponent-result");
+
+const resultText = document.querySelector(".result-text");
 
 // let randomOpponentChoice = getOpponentChoice();
 
@@ -41,44 +44,54 @@ function playRound(playerChoice, opponentChoice) {
 
     opponentScore++;
     opponentResult.textContent = "Opponent result " + opponentScore;
+    resultText.textContent = "You lose! The opponent win with Paper.";
   } else if (playerChoice == 0 && opponentChoice == 2) {
     playerChoiceText.textContent = "Your choice: Rock";
     opponentChoiceText.textContent = "Opponent choice: Scissors";
 
     playerScore++;
     playerResult.textContent = "Your result " + playerScore;
+    resultText.textContent = "You win against the opponent with Rock!";
   } else if (playerChoice == 0 && opponentChoice == 0) {
     playerChoiceText.textContent = "Your choice: Rock";
     opponentChoiceText.textContent = "Opponent choice: Rock";
+    resultText.textContent = "It is a draw this time!";
   } else if (playerChoice == 1 && opponentChoice == 0) {
     playerChoiceText.textContent = "Your choice: Paper";
     opponentChoiceText.textContent = "Opponent choice: Rock";
 
     playerScore++;
     playerResult.textContent = "Your result " + playerScore;
+    resultText.textContent = "You win against the opponent with Paper.";
   } else if (playerChoice == 1 && opponentChoice == 2) {
     playerChoiceText.textContent = "Your choice: Paper";
     opponentChoiceText.textContent = "Opponent choice: Scissors";
 
     opponentScore++;
     opponentResult.textContent = "Opponent result " + opponentScore;
+    resultText.textContent = "You lose! The opponent win with Scissors.";
   } else if (playerChoice == 1 && opponentChoice == 1) {
     playerChoiceText.textContent = "Your choice: Paper";
     opponentChoiceText.textContent = "Opponent choice: Paper";
+    resultText.textContent = "It is a draw this time!";
   } else if (playerChoice == 2 && opponentChoice == 0) {
     playerChoiceText.textContent = "Your choice: Scissors";
     opponentChoiceText.textContent = "Opponent choice: Rock";
 
     opponentScore++;
     opponentResult.textContent = "Opponent result " + opponentScore;
+    resultText.textContent = "You lose! The opponent win with Rock.";
   } else if (playerChoice == 2 && opponentChoice == 1) {
     playerChoiceText.textContent = "Your choice: Scissors";
     opponentChoiceText.textContent = "Opponent choice: Paper";
 
     playerScore++;
     playerResult.textContent = "Your result " + playerScore;
+    resultText.textContent = "You win against the opponent with Scissors.";
   } else if (playerChoice == 2 && opponentChoice == 2) {
     playerChoiceText.textContent = "Your choice: Scissors";
     opponentChoiceText.textContent = "Opponent choice: Scissors";
+    resultText.textContent = "It is a draw this time!";
   }
+  gameRounds++;
 }
